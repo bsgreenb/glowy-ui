a1689d8c7f403959e7800b7f7c5774c55a9b7ba6
Benjamin Greenberg <bsgreenb@gmail.com>
2022-12-26 16:50:28 -0800

    restore main.ts, no need for ... check

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -29,7 +29,7 @@ module.exports = {
 
     const rules = config?.module?.rules?.[0];
 
-    if (rules && rules != "...") {
+    if (rules) {
       rules.exclude = [/node_modules\/(?!(gatsby)\/)/];
     }
 
